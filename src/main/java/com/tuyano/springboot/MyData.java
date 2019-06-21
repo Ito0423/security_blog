6a5e3b2c021ebc2d9e2b9c8e0e60bc67945ee051
Ito <dolkas12@gmail.com>
2019-06-21 11:35:01 +0900

    2019 06 21
    アノテーションクラスのgetter,setterをlombokで省略

--- src/main/java/com/tuyano/springboot/MyData.java
+++ src/main/java/com/tuyano/springboot/MyData.java
@@ -7,15 +7,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Date;
-
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name="mydata")
+@Getter
+@Setter
 public class MyData {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column
+
 	private long id;
 	
 	@Column(length = 50, nullable = true)
@@ -36,50 +40,50 @@ public class MyData {
 	@Column(length = 200, nullable = true)
 	private String title;
 	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getMail() {
-		return mail;
-	}
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public String getMemo() {
-		return memo;
-	}
-	public void setMemo(String memo) {
-		this.memo = memo;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date= date;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
+//	public long getId() {
+//		return id;
+//	}
+//	public void setId(long id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getMail() {
+//		return mail;
+//	}
+//	public void setMail(String mail) {
+//		this.mail = mail;
+//	}
+//
+//	public Integer getAge() {
+//		return age;
+//	}
+//	public void setAge(Integer age) {
+//		this.age = age;
+//	}
+//
+//	public String getMemo() {
+//		return memo;
+//	}
+//	public void setMemo(String memo) {
+//		this.memo = memo;
+//	}
+//	public Date getDate() {
+//		return date;
+//	}
+//	public void setDate(Date date) {
+//		this.date= date;
+//	}
+//	public String getTitle() {
+//		return title;
+//	}
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
 }
